5a36d1e317d5dc3426aa2164bf20aa332b359eb5
prerna-gupta02 <77267840+prerna-gupta02@users.noreply.github.com>
2021-08-11 23:11:02 +0530

    function and responsive updated

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
     $(".bigmem").click(function(){
         if($(this).hasClass("mem1")){
             $(this).toggleClass("big");
+            $(".advisor-btn").toggleClass("left");
             $(".mem2").removeClass("big big1");
             $(".mem26").removeClass("big big2");
         }
         else if($(this).hasClass("mem2")){
             $(this).toggleClass("big big1");
+            $(".founder-btn").toggleClass("top1");
+            $(".UX-btn").toggleClass("top2");
+            $(".mem3").toggleClass("top-right");
             $(".mem1").removeClass("big");
             $(".mem26").removeClass("big big2");
         }
